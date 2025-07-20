e3d03a9bdf2ed2ee1b8d883f20f41a9e4a62f8e7
Sasi Kumar Killi <sasikumarkilli@gmail.com>
2025-07-20 23:29:16 +0530

    implement static search component

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import Search from "./search";
+
+function Header() {
+  return (
+    <div className="p-5 flex">
+      <Search />
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/search.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function Search() {
+  const [searchText, setSearchText] = useState("");
+  const updateText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+  return (
+    <>
+      <form className="w-full">
+        <div className="flex justify-between items-center">
+          <input
+            id="gameSearch"
+            type="seach"
+            placeholder="Search 889,797 games"
+            value={searchText}
+            onChange={updateText}
+            className="w-full appearance-none focus:outline-0 bg-[#ffffff29]
+             rounded-full p-2 pl-10 pr-10 hover:bg-[#fff] hover:text-[#000]
+             bg-[url(src/assets/whiteMagnifying.png)] bg-no-repeat bg-position-[0.7em] bg-center
+             hover:bg-[url(src/assets/blackMagnifying.png)] transition-colors duration-300 ease-in-out"
+          />
+          <div className="flex justify-between absolute right-10 text-[#ffffff80] font-mono">
+            <span className="border-[#ffffff4d] border-1 pl-1 pr-1">alt</span>
+            <span className="ml-2 mr-2">+</span>
+            <span className="border-[#ffffff4d] border-1 pl-1 pr-1">enter</span>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default Search;
